9a35836fdc3d7f183c2c655178c2b8eb7fe06684
Ahmet Tufan <gsdb1905@gmail.com>
2022-09-29 16:54:49 +0300

    app

--- app/src/main/java/com/ahmet/gramer/view/MainActivity.kt
+++ app/src/main/java/com/ahmet/gramer/view/MainActivity.kt
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.work.*
 import com.ahmet.gramer.R
 import com.ahmet.gramer.databinding.ActivityMainBinding
+import com.ahmet.gramer.notification.Notification
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -23,5 +26,31 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment)
         navController = (navHostFragment as NavHostFragment).navController
+
+        myWorkManager()
+    }
+
+    private fun myWorkManager() {
+
+
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(false)
+            .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
+            .setRequiresCharging(false)
+            .setRequiresBatteryNotLow(true)
+            .build()
+
+        val myRequest = PeriodicWorkRequest.Builder(
+            Notification::class.java,
+            12,
+            TimeUnit.HOURS
+        ).setConstraints(constraints).build()
+
+        WorkManager.getInstance(this)
+            .enqueueUniquePeriodicWork(
+                "my_id",
+                ExistingPeriodicWorkPolicy.KEEP,
+                myRequest)
+
     }
 }
